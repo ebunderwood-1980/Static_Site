b7b8c2308df18f4407cbbb6003d072c063c382ba
Cauteryze <e.b.underwood@gmail.com>
2025-01-01 16:36:18 -0800

    HTML Node
    Created HTMLNode class along with a test suite

---
+++ src/test_htmlnode.py
@@ -0,0 +1,11 @@
+import unittest
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
