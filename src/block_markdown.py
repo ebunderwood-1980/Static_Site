b577390582dee84ac924ba9fd4fe1a8704faa3c3
Cauteryze <e.b.underwood@gmail.com>
2025-01-18 20:50:48 -0800

    Block to Block Type
    Uploading to use on desktop.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,4 @@
-
-
+import re
 
 def split_blocks(markdown_text):
     split_text = markdown_text.split("\n\n")
@@ -11,4 +10,42 @@ def split_blocks(markdown_text):
 
     #Return stripped blocks   
     return list(map(lambda x: x.strip(), split_text))
-    
+
+def block_to_block_type(markdown_blocks):
+    results = []
+    for block in markdown_blocks:
+        if re.search(r'^#{1,6}\s{1}[\s\w]+$', block, re.IGNORECASE):
+            results.append('h')
+        elif re.search(r'^`{3}[\s\S]*`{3}$', block, re.IGNORECASE):
+            results.append('c')
+        elif re.search(r'^(>.*)(\n>.*)*$', block, re.IGNORECASE):
+            results.append('q')
+        elif re.search(r'^[*|\-] .*(\n[*|\-] .*)*$', block, re.IGNORECASE):
+            results.append('u')
+        elif re.search(r'^(\d+)\. ', block):
+            lines = block.split('\n')            
+            print(lines)
+            expected_number = 0
+            for line in lines:
+                match = re.match(r'^(\d+)\. ', line)
+                if match:
+                    current_number = int(match.group(1))
+                    if current_number != expected_number + 1:
+                        results.append(None)
+                        break
+                    expected_number = current_number
+                else:
+                    results.append(None)
+                    break
+            results.append('o')
+        else:
+            results.append(None)
+    return results
+        
+# def main():
+#     input = "1. First Item\n2. Second Item"
+#     print(f"Result: {block_to_block_type([input])}")
+
+
+# if __name__ == '__main__':
+#     main()
---
+++ src/test_block_to_block_type.py
@@ -0,0 +1,35 @@
+from block_markdown import block_to_block_type
+import unittest
+
+class TestBlocktoBlockType(unittest.TestCase):
+    def testHeadings(self):
+        inputs = [
+            "# Heading 1",
+            "### Heading 3",
+            "###### Heading 4",
+            "########## This is not a heading",
+            "``` this is a code block```",
+            "``` def test():\n    print(Hello world)```",
+            "``` ```",
+            "```   ``",
+            "``  ```",
+            "> Quote Text",
+            "> Multi-line quote\n> Hello",
+            "> Not a multi-line quote\n Line two",
+            "* First item\n* Second Item",
+            "* First item\n- Second Item",
+            "- First item\n- Second Item",
+            " First item \n- Second Item",
+            "* First item \n  Second item",
+            "*First item",
+            "1. First item\n2. Second item",
+            "0. Zero\n1. First"
+        ]
+
+        function_results = list(map(lambda x: block_to_block_type([x]), inputs))
+        correct_answers = [['h'], ['h'], ['h'], [None], ['c'], ['c'], ['c'], [None], [None], ['q'], ['q'], [None], ['u'], ['u'], ['u'], [None], [None], [None], ['o'], [None]]
+
+        self.assertEqual(function_results, correct_answers)
+        
+
+        
